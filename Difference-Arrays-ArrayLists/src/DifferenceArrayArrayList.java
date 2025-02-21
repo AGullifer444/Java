48bd4a83e8af00b784dd95bb3c1b8e9cf09133b8
Alison Gullifer <aligullifer@gmail.com>
2025-02-20 21:34:45 -0800

    Add files via upload

---
+++ Difference-Arrays-ArrayLists/src/DifferenceArrayArrayList.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+	public class DifferenceArrayArrayList {
+		
+		public static void main(String[] args) {
+			// an array size cannot be modified
+			String [] housePetArray = {"Cat", "Dog", "Bird", "Hamster"}; 
+			// an array will print the memory location of the array
+			System.out.println(housePetArray);
+			// an ArrayList size can be modified
+			ArrayList<String> housePetArrayList = 
+					new ArrayList<>(Arrays.asList("Cat", "Dog", "Bird", "Hamster"));
+			// in an ArrayList the actual data in the elements will print
+			System.out.println(housePetArrayList);
+			
+			//in ArrayList elements can be added, in arrays elements cannot be added
+			housePetArrayList.add("Snake");
+			System.out.println(housePetArrayList);
+			
+			// array & Array List element can be modified by replacing an element
+			System.out.println(housePetArray[1]);
+			housePetArray[1] = "Fish";
+			System.out.println(housePetArray [1]); 
+			housePetArrayList.set(1, "Fish");
+			System.out.println(housePetArrayList);
+			
+			// in ArrayList elements can be removed, an array's size cannot be modified
+			housePetArrayList.remove("Hamster");
+			System.out.println(housePetArrayList);		
+			
+	}
+
+	}
+
